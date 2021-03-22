cdb7ad5c757dcb8cd110beb4f77fb05f34bd59e6
Nopekick <tgkamm@yahoo.com>
2021-03-21 23:41:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [input, changeInput] = useState("")
+  const [output, changeOutput] = useState("Awaiting input and button press...")
+
+  let parse = () => {
+    let num = Number(input)
+
+    if(!isNaN(num)){
+      if(num % 5 === 0 && num % 3 === 0){
+        changeOutput("MustangBronco")
+      } else if(num % 5 === 0){
+        changeOutput("Bronco")
+      } else if(num % 3 === 0){
+        changeOutput("Mustang")
+      } else {
+        changeOutput(num)
+      }
+    } else {
+      changeOutput("Bad input. Please input a number")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="outer">
+      <div id="box">
+        <input onChange={(e)=>{changeInput(e.target.value)}} type="text" placeholder="number..." />
+        <button onClick={()=>{parse()}}>GO</button>
+      </div>
+      <div id="output">{output}</div>
     </div>
   );
 }
